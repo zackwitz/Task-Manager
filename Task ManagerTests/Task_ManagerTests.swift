a5b93cd78bca5a77ad473f20bd3ce9d1662c3d70
zackwitz <zackwitz@gmail.com>
2016-10-17 20:10:59 -0700

    Initial Commit

---
+++ Task ManagerTests/Task_ManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Task_ManagerTests.swift
+//  Task ManagerTests
+//
+//  Created by Zack Hurwitz on 10/17/16.
+//  Copyright © 2016 Zack. All rights reserved.
+//
+
+import XCTest
+@testable import Task_Manager
+
+class Task_ManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
